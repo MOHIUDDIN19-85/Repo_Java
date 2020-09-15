c9aa9b6da60e9bf73be85d848a4ea577df322c73
Technosoft academy 008 <technosoftacademy008@technosoftsmbp2.myfiosgateway.com>
2020-09-15 14:27:41 -0400

    First Homework

---
+++ src/src/Homework/Homework1.java
@@ -0,0 +1,27 @@
+package Homework;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        byte noOfHours = 24;
+        System.out.println("There are " + noOfHours + " hours in a day.");
+        int days = 365;
+        System.out.println("There are " + days + " days in a year.");
+        int noOfEmployes = 577;
+        System.out.println("There are " + noOfEmployes + " people worked in this organization.");
+        int poInCountry = 303247000;
+        System.out.println("Number of population in moon country is " + poInCountry + " .");
+        float intRate = 4.5f;
+        System.out.println("Today's interest rate on my savings deposit is " + intRate  + " .");
+        double balance = 4556.76;
+        System.out.println("Today's balance of my bank account is " + balance + " .");
+        boolean sunRise = true;
+        System.out.println("Does the sun rise from the west ? : No, " + sunRise + " .");
+        sunRise = false;
+        System.out.println("Does the sun rise from the west ? : Yes, " + sunRise );
+        char iniOfFirstName = 'M';
+        char iniOfLastName = 'K';
+        System.out.println("Initials of my name is " + iniOfFirstName +iniOfLastName + " .");
+        String name = "Mohiuddin Khan";
+        System.out.println("My name is " + name + " .");
+    }
+}
