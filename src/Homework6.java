7ebba84442a8df9b51f4590a25174bfc5ab618f2
Technosoft academy 008 <technosoftacademy008@technosoftsmbp2.myfiosgateway.com>
2020-10-09 12:43:24 -0400

    correction1 of H6

--- src/Homework6.java
+++ src/Homework6.java
@@ -1,14 +1,27 @@
 public class Homework6 {
 
-    public static int avg (int num1,int num2,int num3,int num4,int num5){
-        int sum = num1 + num2 + num3+ num4 + num5;
-        int av =sum/5;
-        System.out.println("Sum is: "+ sum + " ,Average Is: "+ av);
-        return av;
+    public static int avg (int[] arr){
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++){
+            sum += arr[i];
+        }
+        int ave = sum / arr.length;
+        return ave;
+    }
+    public static void nameSearch(String[] names, String givenName){
+        for (int i = 0; i < names.length; i++){
+            if (names[i].equals(givenName)){
+                System.out.println("Index of given name is: "+ i);
+            }
+        }
     }
     
     public static void main(String[] args) {
-        int res = avg(22,11,33,44,55);
+        int[] testArray = {22,11,33,44,55};
+        System.out.println("Average of given array is: "+ avg(testArray));
+        String[] values = {"John", "michael","gphilippre","phlip","ilena","palena","fahry"};
+        nameSearch(values,"ilena");
+
     }
 
 }
