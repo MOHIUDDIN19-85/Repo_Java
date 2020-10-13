b2dd1741a0c884ec8ae7ee822e7b4ab4788a3351
Technosoft academy 008 <technosoftacademy008@technosoftsmbp2.myfiosgateway.com>
2020-10-13 13:57:48 -0400

    Homewrk8

--- src/Homework8.java
+++ src/Homework8.java
@@ -1,2 +1,13 @@
 public class Homework8 {
+    public static void main(String[] args) {
+        MethodH8 m1 = new MethodH8();
+      m1.printSinglepg(15);
+      m1.printDoublePg(30);
+      MethodH8.printDoublePg(46);
+      MethodH8.printSinglepg(2);
+      m1.checkToner();
+
+
+
+    }
 }
