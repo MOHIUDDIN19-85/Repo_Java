a73d30ea9d9228d9f7a91644eab658e7b4554492
Technosoft academy 008 <technosoftacademy008@technosoftsmbp2.myfiosgateway.com>
2020-11-04 15:15:42 -0500

    Homework-9

---
+++ src/Homework9.java
@@ -0,0 +1,16 @@
+public class Homework9 {
+    public static void main(String[] args) {
+        Restaurent g1 = new Restaurent();
+        g1.seatAvailable();
+        g1.seatsParty(5);
+        g1.removeParty(5);
+        g1.summary_of_restaurent();
+        g1.summary_of_restaurent();
+        Restaurent g2 = new Restaurent();
+        g2.summary_of_restaurent();
+        g2.seatAvailable();
+
+
+    }
+
+}
---
+++ src/Restaurent.java
@@ -0,0 +1,60 @@
+import apple.laf.JRSUIConstants;
+
+import java.util.Scanner;
+
+public class Restaurent {
+    public String restaurentName = "Burger King";
+    public int guestCapasity = 100;
+    public static int guestCount = 0;
+    public static int availableSeats =100 ;
+    public static int guests;
+
+    public Restaurent() {
+
+    }
+
+
+    //P1
+    public void seatAvailable(){
+        System.out.println("Available number of seats are: "+ availableSeats);
+        Scanner seat = new Scanner(System.in);
+        System.out.println("Please enter the number of guest: ");
+        this.guests = seat.nextInt();
+        if (guests<=availableSeats){
+            System.out.println("Wellcome to our restaurent");
+            guestCount = guestCount + guests;
+            availableSeats = availableSeats - guestCount;
+        }
+        else{
+            System.out.println("Please wait for a moment,\nwhen we are available you are most wellcome");
+        }
+
+
+    }
+    //P2
+    public void seatsParty(int guestNum){
+        int num = guestNum;
+        if (num <= availableSeats){
+            System.out.println("Please be seated and enjoy the meal");
+        }
+        else {
+            System.out.println("Wait for a moment, Soon we will be available");
+        }
+    }
+    //P-3
+    public void removeParty(int rm){
+        availableSeats = availableSeats + rm;
+        System.out.println("Enjoy your time,Come again");
+    }
+//P-4
+    public Restaurent(String name, String guestcapacity){
+        name = restaurentName;
+        guestcapacity = guestcapacity;
+    }
+    //p-5
+    public void summary_of_restaurent(){
+        System.out.println("Name of the restaurent is: "+ restaurentName);
+        System.out.println("Current guest count is: "+ guestCount);
+        System.out.println("Guest capacity is: "+ guestCapasity);
+    }
+}
