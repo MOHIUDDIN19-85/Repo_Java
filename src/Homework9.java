f4e45d425eb0e0a2c87efc7e524f2b9474d30599
Technosoft academy 008 <technosoftacademy008@technosoftsmbp2.myfiosgateway.com>
2020-11-04 16:17:10 -0500

    H9-1

--- src/Homework9.java
+++ src/Homework9.java
@@ -2,18 +2,10 @@ public class Homework9 {
     public static void main(String[] args) {
         Restaurent g1 = new Restaurent();
         g1.seatAvailable();
-        g1.seatsParty(6);
-        g1.removeParty(6);
-        g1.summary_of_restaurent();
         g1.summary_of_restaurent();
         Restaurent g2 = new Restaurent();
-        g2.summary_of_restaurent();
         g2.seatAvailable();
-        Restaurent g3 = new Restaurent();
-        g3.summary_of_restaurent();
-        g3.seatAvailable();
-
-
+        g2.summary_of_restaurent();
+    }
     }
 
-}
